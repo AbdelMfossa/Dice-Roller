da4182b96193cca4a7ec514b577ca1478c65d2f6
Abdel Mfossa <abdelemfossa@gmail.com>
2020-12-20 20:04:12 +0100

    Use the dice images

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.diceroller
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
@@ -31,11 +32,11 @@ class MainActivity : AppCompatActivity() {
     private fun rollDice() {
         // Create new Dice object with 6 sides and roll it
         val dice = Dice(6)
-//        val diceRoll = dice.roll()
 
         // Update the screen with the dice roll
-        val resultTextView: TextView = findViewById(R.id.textView)
-        resultTextView.text = dice.roll().toString()
+        val diceImage: ImageView = findViewById(R.id.imageView2)
+        diceImage.setImageResource(R.drawable.dice_2)
+        // resultTextView.text = dice.roll().toString()
     }
 }
 
